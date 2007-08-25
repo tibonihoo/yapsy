de00a7c7be459fec29046d23e925a1fc11cbea63
tibonihoo <none@none>
2007-08-25 17:39:21 +0000

    preparing the first release of yapsy
    --HG--
    extra : convert_revision : svn%3A3e6e16ca-9737-0410-a443-d01c68b3c16e%4033

---
+++ yapsydir/branches/1.0/test/ConfigPlugin.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+
+"""
+This is certainly the second simplest plugin ever.
+"""
+
+import test_settings
+import main
+from yapsy.IPlugin import IPlugin
+
+class ConfigPlugin(IPlugin):
+	"""
+	Try to use the methods with which it has been decorated.
+	"""
+
+	def __init__(self):
+		"""
+		init
+		"""
+		# initialise parent class
+		IPlugin.__init__(self)
+
+
+	def activate(self):
+		"""
+		Call the parent class's acivation method
+		"""
+		IPlugin.activate(self)
+		return
+
+
+	def deactivate(self):
+		"""
+		Just call the parent class's method
+		"""
+		IPlugin.deactivate(self)
+
+
+	def choseTestOption(self, value):
+		"""
+		Set an option to a given value.
+		"""
+		self.setConfigOption("Test",value)
+
+	def checkTestOption(self):
+		"""
+		Test if the test option is here.
+		"""
+		return self.hasConfigOption("Test")
+
+	def getTestOption(self):
+		"""
+		Return the value of the test option.
+		"""
+		return self.getConfigOption("Test")
+
+
---
+++ yapsydir/branches/1.0/test/test_settings.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+sys.path.append(
+	os.path.dirname(
+		os.path.dirname(
+			os.path.abspath(__file__))))
+
+sys.path.append(
+	os.path.dirname(
+		os.path.dirname(
+			os.path.dirname(
+				os.path.abspath(__file__)))))
+
