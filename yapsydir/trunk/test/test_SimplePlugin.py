090a3e2ae98eb0e8910d71e503189da8bc570f0f
tibonihoo <none@none>
2007-09-23 15:07:50 +0000

    The singleton tests disappears !
    --HG--
    extra : convert_revision : svn%3A3e6e16ca-9737-0410-a443-d01c68b3c16e%4051

--- yapsydir/trunk/test/test_SimplePlugin.py
+++ yapsydir/trunk/test/test_SimplePlugin.py
@@ -2,8 +2,7 @@ import test_settings
 import unittest
 import os 
 
-from yapsy.PluginManager import PluginManager, PluginManagerSingleton
-#from yapsy.IPlugin import IPlugin
+from yapsy.PluginManager import PluginManager
 
 
 class SimpleTestsCase(unittest.TestCase):
@@ -63,66 +62,6 @@ class SimpleTestsCase(unittest.TestCase):
 
 
 
-class SimpleSingletonTestsCase(unittest.TestCase):
-	"""
-	Test the correct loading of a simple plugin as well as basic
-	commands, use the Singleton version of the PluginManager.
-	"""
-	
-	def setUp(self):
-		"""
-		init
-		"""
-		# create the plugin manager
-		simplePluginManager = PluginManagerSingleton.get()
-		simplePluginManager.setPluginPlaces(directories_list=[os.path.dirname(os.path.abspath(__file__))])
-		# load the plugins that may be found
-		simplePluginManager.collectPlugins()
-		# Will be used later
-		self.plugin_info = None
-
-	def plugin_loading_check(self):
-		"""
-		Test if the correct plugin has been loaded.
-		"""
-		if self.plugin_info is None:
-			simplePluginManager = PluginManagerSingleton.get()
-			# check nb of categories
-			self.assertEqual(len(simplePluginManager.getCategories()),1)
-			sole_category = simplePluginManager.getCategories()[0]
-			# check the number of plugins
-			self.assertEqual(len(simplePluginManager.getPluginsOfCategory(sole_category)),1)
-			self.plugin_info = simplePluginManager.getPluginsOfCategory(sole_category)[0]
-			# test that the name of the plugin has been correctly defined
-			self.assertEqual(self.plugin_info.name,"Simple Plugin")
-			self.assertEqual(sole_category,self.plugin_info.category)
-		else:
-			self.assert_(True)
-
-	def testLoaded(self):
-		"""
-		Test if the correct plugin has been loaded.
-		"""
-		self.plugin_loading_check()
-		
-
-	def testActivationAndDeactivation(self):
-		"""
-		Test if the activation procedure works.
-		"""
-		self.plugin_loading_check()
-		simplePluginManager = PluginManagerSingleton.get()
-		self.assert_(not self.plugin_info.plugin_object.is_activated)
-		simplePluginManager.activatePluginByName(self.plugin_info.category,
-												 self.plugin_info.name)
-		self.assert_(self.plugin_info.plugin_object.is_activated)
-		simplePluginManager.deactivatePluginByName(self.plugin_info.category,
-												   self.plugin_info.name)
-		self.assert_(not self.plugin_info.plugin_object.is_activated)
-
-
-
 suite = unittest.TestSuite([
 		unittest.TestLoader().loadTestsFromTestCase(SimpleTestsCase),
-		unittest.TestLoader().loadTestsFromTestCase(SimpleSingletonTestsCase),
 		])
