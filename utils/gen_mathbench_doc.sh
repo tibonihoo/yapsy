343cffb3bfc8a013ac2c63c04ab49b538966c9c8
tibonihoo <none@none>
2007-10-23 19:09:58 +0000

    only yapsy here now
    --HG--
    extra : convert_revision : svn%3A3e6e16ca-9737-0410-a443-d01c68b3c16e%4078

--- utils/gen_mathbench_doc.sh
+++ utils/gen_mathbench_doc.sh
@@ -5,22 +5,17 @@
 export PYTHONPATH=$HOME/libs/docutils-0.4:$HOME/libs/docutils-0.4/extras:$PYTHONPATH
 
 # the sources of all sources
-PROJECT_CODE_REPOS=https://mathbench.svn.sourceforge.net/svnroot/mathbench
+PROJECT_CODE_REPOS=https://yapsy.svn.sourceforge.net/svnroot/yapsy
 YAPSY_SRC_REPO=yapsydir/trunk/
-MATHBENCH_SRC_REPO=mathbenchdir/trunk/
 
 # the destination of the doc
-DOC_DEST=/home/groups/m/ma/mathbench/htdocs
+DOC_DEST=/home/groups/y/ya/yapsy/htdocs
 YAPSY_DEST=yapsy
-MATHBENCH_DEST=mathbench
 
 # get the latest version of the docs
 svn co $PROJECT_CODE_REPOS/$YAPSY_SRC_REPO/doc $DOC_DEST/$YAPSY_DEST/doc
-svn co $PROJECT_CODE_REPOS/$MATHBENCH_SRC_REPO/doc $DOC_DEST/$MATHBENCH_DEST/doc
 svn co  $PROJECT_CODE_REPOS/$YAPSY_SRC_REPO/artwork $DOC_DEST/$YAPSY_DEST/artwork
-svn co $PROJECT_CODE_REPOS/$MATHBENCH_SRC_REPO/artwork $DOC_DEST/$MATHBENCH_DEST/artwork
 
 # generate the doc
 cd $DOC_DEST/$YAPSY_DEST/doc && for f in `ls *rst`; do rst2html $f `basename $f .rst`.html; done;
-cd $DOC_DEST/$MATHBENCH_DEST/doc && for f in `ls *rst`; do rst2html $f `basename $f .rst`.html; done;
 
