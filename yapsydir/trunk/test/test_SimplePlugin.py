4625cedb2976aecc08749a9764a26e8b500c18c6
tibonihoo <none@none>
2010-06-26 22:27:51 +0000

    - test new methods of the PluginManager - adapt test to VersionPluginManager's changes
    --HG--
    extra : convert_revision : svn%3A3e6e16ca-9737-0410-a443-d01c68b3c16e%40120

--- yapsydir/trunk/test/test_SimplePlugin.py
+++ yapsydir/trunk/test/test_SimplePlugin.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8; tab-width: 4; indent-tabs-mode: t -*-
+
 import test_settings
 import unittest
 import os 
@@ -62,8 +64,59 @@ class SimpleTestsCase(unittest.TestCase):
 		self.assert_(not self.plugin_info.plugin_object.is_activated)
 
 
+class SimplePluginAdvancedManipulationTestsCase(unittest.TestCase):
+	"""
+	Test some advanced manipulation on the core data of a PluginManager.
+	"""
+	
+        
+	def testCategoryManipulation(self):
+		"""
+		Test querying, removing and adding plugins from/to a category.
+		"""
+		spm = PluginManager(directories_list=[
+				os.path.join(
+					os.path.dirname(os.path.abspath(__file__)),"plugins")])
+		# load the plugins that may be found
+		spm.collectPlugins()
+		# check that the getCategories works
+		self.assertEqual(len(spm.getCategories()),1)
+        sole_category = spm.getCategories()[0]
+		# check the getPluginsOfCategory
+        self.assertEqual(len(spm.getPluginsOfCategory(sole_category)),1)
+        plugin_info = spm.getPluginsOfCategory(sole_category)[0]
+		# try to remove it and check that is worked
+		spm.removePluginFromCategory(plugin_info,sole_category)
+        self.assertEqual(len(spm.getPluginsOfCategory(sole_category)),0)
+		# now re-add this plugin the to same category
+		spm.appendPluginToCategory(plugin_info,sole_category)
+		self.assertEqual(len(spm.getPluginsOfCategory(sole_category)),1)
 
+	def testCandidatesManipulation(self):
+		"""
+		Test querying, removing and adding plugins from/to the lkist
+		of plugins to load.
+		"""
+		spm = PluginManager(directories_list=[
+				os.path.join(
+					os.path.dirname(os.path.abspath(__file__)),"plugins")])
+		# locate the plugins that should be loaded
+		spm.locatePlugins()
+		# check nb of candidatesx
+		self.assertEqual(len(spm.getPluginCandidates()),1)
+		# get the description of the plugin candidate
+		candidate = spm.getPluginCandidates()[0]
+		self.assertTrue(isinstance(candidate,tuple))
+		# try removing the candidate
+		spm.removePluginCandidate(candidate)
+		self.assertEqual(len(spm.getPluginCandidates()),0)
+		# try re-adding it
+		spm.appendPluginCandidate(candidate)
+		self.assertEqual(len(spm.getPluginCandidates()),1)
+				
+		
 
 suite = unittest.TestSuite([
 		unittest.TestLoader().loadTestsFromTestCase(SimpleTestsCase),
+		unittest.TestLoader().loadTestsFromTestCase(SimplePluginAdvancedManipulationTestsCase)
 		])
--- yapsydir/trunk/test/test_VersionedPlugin.py
+++ yapsydir/trunk/test/test_VersionedPlugin.py
@@ -1,4 +1,5 @@
-from test_settings import *
+import test_settings
+from test_settings import TEST_MESSAGE
 import unittest
 import os 
 
@@ -16,7 +17,6 @@ class VersionedTestsCase(unittest.TestCase):
         init
         """
         # create the plugin manager
-#        print os.path.join(os.path.dirname(os.path.abspath(__file__)),"plugins")
         self.versionedPluginManager = VersionedPluginManager(
             directories_list=[os.path.join(
 					os.path.dirname(os.path.abspath(__file__)),"plugins")],
@@ -35,8 +35,11 @@ class VersionedTestsCase(unittest.TestCase):
             # check nb of categories
             self.assertEqual(len(self.versionedPluginManager.getCategories()),1)
             sole_category = self.versionedPluginManager.getCategories()[0]
-            # check the number of plugins
-            self.assertEqual(len(self.versionedPluginManager.getPluginsOfCategory(sole_category)),5)
+            # check the number of plugins (the older versions of the
+            # plugins should not be there)
+            self.assertEqual(len(self.versionedPluginManager.getPluginsOfCategory(sole_category)),1)
+            # older versions of the plugin should be found in the attic
+            self.assertEqual(len(self.versionedPluginManager.getPluginsOfCategoryFromAttic(sole_category)),4)
             plugins = self.versionedPluginManager.getPluginsOfCategory(sole_category)
             self.plugin_info = None
             for plugin_info in plugins:
@@ -48,14 +51,6 @@ class VersionedTestsCase(unittest.TestCase):
             # test that the name of the plugin has been correctly defined
             self.assertEqual(self.plugin_info.name,"Versioned Plugin")
             self.assertEqual(sole_category,self.plugin_info.category)
-            
-            self.assertEqual(len(self.versionedPluginManager.getLatestPluginsOfCategory(sole_category)),1)
-            self.plugin_info = self.versionedPluginManager.getLatestPluginsOfCategory(sole_category)[0]
-            TEST_MESSAGE("plugin info: %s" % self.plugin_info)
-            # test that the name of the plugin has been correctly defined
-            self.assertEqual(self.plugin_info.name,"Versioned Plugin")
-            self.assertEqual(sole_category,self.plugin_info.category)
-            self.assertEqual("1.2",str(self.plugin_info.version))
         else:
             self.assert_(True)
 
@@ -64,22 +59,55 @@ class VersionedTestsCase(unittest.TestCase):
         Test if the correct plugin has been loaded.
         """
         self.plugin_loading_check()
-        
+        sole_category = self.versionedPluginManager.getCategories()[0]
+        self.assertEqual(len(self.versionedPluginManager.getLatestPluginsOfCategory(sole_category)),1)
+        self.plugin_info = self.versionedPluginManager.getLatestPluginsOfCategory(sole_category)[0]
+        TEST_MESSAGE("plugin info: %s" % self.plugin_info)
+        # test that the name of the plugin has been correctly defined
+        self.assertEqual(self.plugin_info.name,"Versioned Plugin")
+        self.assertEqual(sole_category,self.plugin_info.category)
+        self.assertEqual("1.2",str(self.plugin_info.version))
 
+        
+    def testLatestPluginOfCategory(self):
+        self.plugin_loading_check()
+        
     def testActivationAndDeactivation(self):
         """
         Test if the activation procedure works.
         """
         self.plugin_loading_check()
         self.assert_(not self.plugin_info.plugin_object.is_activated)
-        TEST_MESSAGE("plugin object = %s" % self.plugin_info.plugin_object)
-        self.plugin_info.plugin_object.activate()
+        self.versionedPluginManager.activatePluginByName(self.plugin_info.name,
+                                                         self.plugin_info.category)
         self.assert_(self.plugin_info.plugin_object.is_activated)
-        self.plugin_info.plugin_object.deactivate()
+        self.versionedPluginManager.deactivatePluginByName(self.plugin_info.name,
+                                                           self.plugin_info.category)
         self.assert_(not self.plugin_info.plugin_object.is_activated)
+        # also check that this is the plugin of the latest version
+        # that has been activated (ok the following test is already
+        # ensured by the plugin_loading_check method, but this is to
+        # make the things clear: the plugin chosen for activation is
+        # the one with the latest version)
+        self.assertEqual("1.2",str(self.plugin_info.version))
+        
+        
+    # def testDirectActivationAndDeactivation(self):
+    #     """
+    #     Test if the activation procedure works when directly activating a plugin.
+    #     """
+    #     self.plugin_loading_check()
+    #     self.assert_(not self.plugin_info.plugin_object.is_activated)
+    #     TEST_MESSAGE("plugin object = %s" % self.plugin_info.plugin_object)
+    #     self.plugin_info.plugin_object.activate()
+    #     self.assert_(self.plugin_info.plugin_object.is_activated)
+    #     self.plugin_info.plugin_object.deactivate()
+    #     self.assert_(not self.plugin_info.plugin_object.is_activated)
 
 
 
+    
+        
 suite = unittest.TestSuite([
         unittest.TestLoader().loadTestsFromTestCase(VersionedTestsCase),
         ])
