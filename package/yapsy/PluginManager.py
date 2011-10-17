55d804b326a47329880e8c7551ed1050e018287a
Mathieu Clabaut <mathieu.clabaut@gmail.com>
2011-10-17 22:39:12 +0200

    Allows plugin instance to be referenced in several categories at once.

--- package/yapsy/PluginManager.py
+++ package/yapsy/PluginManager.py
@@ -459,24 +459,24 @@ class PluginManager(object):
 			# now try to find and initialise the first subclass of the correct plugin interface
 			for element in candidate_globals.itervalues():
 				current_category = None
+				plugin_info_reference = None
 				for category_name in self.categories_interfaces:
 					try:
 						is_correct_subclass = issubclass(element, self.categories_interfaces[category_name])
-					except:
+					except TypeError:
 						continue
-					if is_correct_subclass:
-						if element is not self.categories_interfaces[category_name]:
-							current_category = category_name
-							break
-				if current_category is not None:
-					if not (candidate_infofile in self._category_file_mapping[current_category]): 
-						# we found a new plugin: initialise it and search for the next one
-						plugin_info.plugin_object = element()
-						plugin_info.category = current_category
-						self.category_mapping[current_category].append(plugin_info)
-						self._category_file_mapping[current_category].append(candidate_infofile)
-						current_category = None
-					break
+					if is_correct_subclass and \
+							element is not self.categories_interfaces[category_name]:
+						current_category = category_name
+						if not (candidate_infofile in self._category_file_mapping[current_category]): 
+							# we found a new plugin: initialise it and search for the next one
+							if not plugin_info_reference:
+								plugin_info.plugin_object = element()
+								plugin_info.category = current_category
+								plugin_info_reference = plugin_info
+							self.category_mapping[current_category].append(plugin_info_reference)
+							self._category_file_mapping[current_category].append(candidate_infofile)
+							current_category = None
 
 		# Remove candidates list since we don't need them any more and
 		# don't need to take up the space
