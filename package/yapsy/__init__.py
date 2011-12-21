b3430cbeb1d774c92a225bd24643aa90dbcdf71a
thibauld@localhost6.localdomain6 <thibauld@localhost6.localdomain6>
2011-12-21 16:33:31 +0100

    Add some more code samples and explicit notes about how to communicate with a plugin once it is loaded

--- package/yapsy/__init__.py
+++ package/yapsy/__init__.py
@@ -36,7 +36,18 @@ should get you a fully working plugin management system::
    for pluginInfo in simplePluginManager.getAllPlugins():
       simplePluginManager.activatePluginByName(pluginInfo.name)
    
-   
+.. note:: The ``plugin_info`` object (typically an instance of
+``IPlugin``) plays as *the entry point of each plugin*. That's also
+where |yapsy| ceases to guide you: it's up to you to define what your
+plugins can do and how you want to talk to them ! Talking to your
+plugin will then look very much like the following::
+
+   # Trigger 'some action' from the loaded plugins
+   for pluginInfo in simplePluginManager.getAllPlugins():
+      pluginInfo.plugin_object.doSomething(...)
+
+
+
 .. _extend:
 
 Extensibility
@@ -74,7 +85,14 @@ the manager about that before collecting plugins::
       "Visualization" : IVisualisation,
       })
     
+.. note:: Communicating with the plugins belonging to a given category
+might then be achieved with some code looking like the following::
 
+   # Trigger 'some action' from the "Visualization" plugins 
+   for pluginInfo in simplePluginManager.getPluginsOfCategory("Visualization"):
+      pluginInfo.plugin_object.doSomething(...)
+
+      
 Enhance the manager's interface
 -------------------------------
 
