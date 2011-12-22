327500667932caf9240bedbcf5bdbbd033795b98
thibauld@localhost6.localdomain6 <thibauld@localhost6.localdomain6>
2011-12-22 22:57:23 +0100

    correct zip invocation

--- utils/gen_doc.sh
+++ utils/gen_doc.sh
@@ -8,4 +8,4 @@ make clean html
 # upload the documentation to SourceForge
 scp -r _build/html/* tibonihoo,yapsy@web.sourceforge.net:/home/groups/y/ya/yapsy/htdocs/
 # create a zip ready to be released
-cd _build && zip yapsy_doc html
+cd _build && zip -r yapsy_doc html
