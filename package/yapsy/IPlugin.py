469c917d890a12a2ce0e5383d64c237b8a829c00
thibauld@koala-ultrabook <thibauld@koala-ultrabook>
2012-04-07 09:34:28 +0200

    correct header of python source files

--- package/yapsy/IPlugin.py
+++ package/yapsy/IPlugin.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8; tab-width: 4; indent-tabs-mode: t -*-
 
 
 """
--- package/yapsy/__init__.py
+++ package/yapsy/__init__.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8; tab-width: 4; indent-tabs-mode: t -*-
 """
 
 Overview
