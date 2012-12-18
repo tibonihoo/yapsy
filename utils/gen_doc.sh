ffd00c9aff4a4e578beaa469c83bf3d2e4519509
Thibauld Nion <thibauld@tibonihoo.net>
2012-12-18 21:12:22 +0100

    update upload path (follwoing SF upgrade)

--- utils/gen_doc.sh
+++ utils/gen_doc.sh
@@ -6,6 +6,6 @@ cd ../package/doc/
 # compile the documentation
 make clean html
 # upload the documentation to SourceForge
-scp -r _build/html/* tibonihoo,yapsy@web.sourceforge.net:/home/groups/y/ya/yapsy/htdocs/
+scp -r _build/html/* tibonihoo@web.sourceforge.net:/home/project-web/yapsy/htdocs/
 # create a zip ready to be released
 cd _build && zip -r yapsy_doc html
