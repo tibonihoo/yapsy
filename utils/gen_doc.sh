4db540e9c23879326f1b6ac9e6751b981da78fbb
Thibauld Nion <thibauld@tibonihoo.net>
2013-05-22 22:00:37 +0200

    small improvement to doc gen script

--- utils/gen_doc.sh
+++ utils/gen_doc.sh
@@ -5,7 +5,12 @@
 cd ../package/doc/
 # compile the documentation
 make clean html
-# upload the documentation to SourceForge
-scp -r _build/html/* tibonihoo@web.sourceforge.net:/home/project-web/yapsy/htdocs/
-# create a zip ready to be released
-cd _build && zip -r yapsy_doc html
+
+if [ "$1" = "upload" ]; then
+  # upload the documentation to SourceForge
+  scp -r _build/html/* tibonihoo@web.sourceforge.net:/home/project-web/yapsy/htdocs/
+  # create a zip ready to be released
+  cd _build && zip -r yapsy_doc html
+  echo "Documentation is ready to be uploaded to python.org"
+fi;
+
