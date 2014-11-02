3fa50c66f7d0523cb2f1f6fa8c75a87e42658ca6
Thibauld Nion <thibauld@tibonihoo.net>
2014-11-02 22:08:15 +0100

    remove obsolete scripts and fix manifest and setup.py

--- package/setup.py
+++ package/setup.py
@@ -59,6 +59,7 @@ try:
 					 'License :: OSI Approved :: BSD License',
 					 'Operating System :: OS Independent',
 					 'Programming Language :: Python',
+					 'Programming Language :: Python :: 2',
 					 'Programming Language :: Python :: 3',
 					 'Topic :: Software Development :: Libraries :: Python Modules'],
 		platforms='All',
