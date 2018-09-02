fe31c55e2fb3eeed2b1b3ecec585ffb2d413ef18
Thibauld Nion <thibauld@tibonihoo.net>
2018-09-02 18:45:39 +0200

    Use twine to upload to the new pypi

--- utils/upload_packages.sh
+++ utils/upload_packages.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 # -*- coding: utf-8 -*-
 
+# MAKE SURE TO RUN THE FOLLOWING UPDATE
+# (taken from https://stackoverflow.com/questions/45207128/failed-to-upload-packages-to-pypi-410-gone)
+#pip install -U pip setuptools twine
+
 # Get the name of the directory containing this script
 # Note: little trick taken from  http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
 THIS_FILE_DIR=$( cd "$( dirname $0 )" && pwd )
 
 cd $THIS_FILE_DIR/../package
 
+
 # generate package
 rm -r build 
 rm -r dist
-python setup.py sdist bdist_egg upload
-python3 setup.py bdist_egg upload
+python setup.py sdist bdist_egg
+python3 setup.py bdist_egg
+twine upload dist/*
+
