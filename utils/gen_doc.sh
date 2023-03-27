81e6c49344597b211417f3a7497149d559dba8f8
Thibauld Nion <thibauld@tibonihoo.net>
2023-03-27 21:08:57 +0200

    Remove ability to upload doc to sourceforge.

--- utils/gen_doc.sh
+++ utils/gen_doc.sh
@@ -11,11 +11,5 @@ rm -r $PACKAGE_DIR/build
 # Generate doc
 python $PACKAGE_DIR/setup.py build_sphinx
 
-# Upload it if requested
-if [ "$1" == "upload" ]; then
-  echo "Uploading to Sourceforge"
-  scp -r $PACKAGE_DIR/build/sphinx/html/* tibonihoo@web.sourceforge.net:/home/project-web/yapsy/htdocs/
-fi;
-
 # Exit release environment
 deactivate_release_env
